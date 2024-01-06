8d52463521160619d11a4a6e95b6774b1d42983a
Krishna Pramod <krishna.pramod2@gmail.com>
2024-01-06 16:06:03 +0530

    Added a few keymaps and sets

--- init.lua
+++ init.lua
@@ -101,6 +101,11 @@ cmp.setup({
 
 --Custom Keymaps
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>sp', vim.cmd.vsplit)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Telescope keymaps
 local builtin = require('telescope.builtin')
@@ -116,3 +121,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.nu = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
